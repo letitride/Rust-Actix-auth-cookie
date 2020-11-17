e23447ad28d6f58241eafea5d7b6d6fc7afd5e25
letitride <ichikawa.fumiya@gmail.com>
2020-11-17 09:31:49 +0900

    actixでの認証cookie検証
    PlayやRailsのように"暗号化"されたクレデンシャルをcookieにセットする

---
+++ reaeme.md
@@ -0,0 +1,1 @@
+$ curl -X POST -d "email=ichikawa.fumiya@gmail.com" localhost:3000/login
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
+use actix_identity::{Identity, CookieIdentityPolicy, IdentityService};
 use rand::Rng;
+use serde::{Deserialize, Serialize};
+use log::debug;
 
-async fn index() -> String {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LoginForm {
+    email: String,
+}
+
+async fn index(id:Identity) -> String {
     format!(
         "Hello {}",
-        "world".to_string()
+        id.identity().unwrap_or_else(|| "Anonymous".to_owned())
     )
+}
+
+async fn login(params: web::Form<LoginForm>, id: Identity) -> HttpResponse {
+    id.remember( params.email.clone() );
+    HttpResponse::Found().header("location", "/").finish()
+}
+
+async fn authonly(id: Identity) -> HttpResponse {
+    match id.identity() {
+        Some(id) => HttpResponse::Ok().body( format!("success {}", id) ),
+        None => HttpResponse::Found().header("location", "/").finish(),
+    }
 }
 
 #[actix_web::main]
@@ -16,7 +36,15 @@ async fn main() -> std::io::Result<()> {
     let private_key = rand::thread_rng().gen::<[u8; 32]>();
     HttpServer::new( move || { 
         App::new()
+        .wrap(middleware::Logger::default())
+        .wrap(IdentityService::new(
+            CookieIdentityPolicy::new(&private_key)
+                .name("auth-example")
+                .secure(false),
+        ))
         .service(web::resource("/").route(web::get().to(index)))
+        .service(web::resource("/login").route(web::post().to(login)))
+        .service(web::resource("/authonly").route(web::get().to(authonly)))
     })
     .bind("localhost:3000")?
     .run()
